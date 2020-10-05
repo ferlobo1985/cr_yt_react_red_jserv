135fdd098d4cbae5a3f45efad4d637323703191d
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-05 17:27:25 -0300

    4-Showing post masonry

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,7 +2,11 @@ import React,{ useEffect } from 'react';
 import { useDispatch,  useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 
+import { Container, Card, Button } from 'react-bootstrap';
+
 import { getPosts } from '../../store/actions';
+import Masonry from 'react-masonry-css';
+import { scryRenderedDOMComponentsWithClass } from 'react-dom/test-utils';
 
 const Home = () => {
     const posts = useSelector(state => state.posts);
@@ -12,10 +16,40 @@ const Home = () => {
         dispatch(getPosts());
     },[dispatch])
 
+    const renderPosts = () => (
+        posts.list ?
+            posts.list.map((item,i)=>(
+                <Card key={i}>
+                    <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Text>
+                            {item.excerpt}
+                        </Card.Text>
+
+                        <LinkContainer to={`/post/${item.id}`}>
+                            <Button variant="light">Read article</Button>
+                        </LinkContainer>
+                    </Card.Body>
+                </Card>
+            ))
+        :null
+    )
+
+
     return(
-        <div>
-            Home
-        </div>
+        <Container>
+
+            <div className="mt-3">
+                <Masonry
+                    breakpointCols={3}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"
+                >
+                    { renderPosts() }
+                </Masonry>
+            </div>
+
+        </Container>
     )
 }
 
