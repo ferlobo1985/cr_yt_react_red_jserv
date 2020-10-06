717c26068c16342d85fd9092ee58d68c5b137859
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-05 22:14:14 -0300

    Creating a contact for part 1

---
+++ src/components/contact/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import { Form, Button, Alert, Container } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+
+const Contact = () => {
+    const dispatch = useDispatch();
+    const formik = useFormik({
+        initialValues:{
+            name:'',
+            email:'',
+            message:''
+        },
+        validationSchema:Yup.object({
+            name: Yup.string().required('Sorry the name is required'),
+            email: Yup.string()
+            .email('Invalid email')
+            .required('Sorry the email is required'),
+            message: Yup.string()
+            .required('Sorry the message is required')
+        }),
+        onSubmit: ( values )=>{
+            console.log(values)
+        }
+    })
+
+    return(
+        <Container className="mt-4">
+            <h1>Contact us</h1>
+            <Form onSubmit={formik.handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type="string"
+                        name="name"
+                        placeholder="Enter your name"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                    />
+                    { formik.touched.name && formik.errors.name ?
+                        <Alert variant="danger">
+                            { formik.errors.name }
+                        </Alert>
+                    :null}
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                     { formik.touched.email && formik.errors.email ?
+                        <Alert variant="danger">
+                            { formik.errors.email }
+                        </Alert>
+                    :null}
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows="3"
+                        name="message"
+                        placeholder="Enter your email"
+                        onChange={formik.handleChange}
+                        value={formik.values.message}
+                    />
+                    { formik.touched.message && formik.errors.message ?
+                        <Alert variant="danger">
+                            { formik.errors.message }
+                        </Alert>
+                    :null}
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+
+
+            </Form>
+        </Container>
+    )
+}
+
+export default Contact;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/home';
 import Post from './components/post'
+import Contact from './components/contact';
 
 
 const Routes = () => (
@@ -11,6 +12,7 @@ const Routes = () => (
         <Header/>
         <Switch>
             <Route path="/post/:id" component={Post}/>
+            <Route path="/contact" component={Contact}/>
             <Route path="/" component={Home}/>
         </Switch>
     </BrowserRouter>
